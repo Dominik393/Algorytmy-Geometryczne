e9e7dd238c9568e166cf8eae377fb69fd9d4d7c6
Dominik393 <Dominiksmykow@wp.pl>
2023-12-13 13:26:14 +0100

    So close to working

--- lab4/Miotla.py
+++ lab4/Miotla.py
@@ -6,8 +6,8 @@ def find_intersections(lines):
     q = PriorityQueue()
 
     for i, line in enumerate(lines):
-        q.put(Event(line.start.x, 1, i))
-        q.put(Event(line.end.x, 0, i))
+        q.put(Event(line.start.x, 1, (i,)))
+        q.put(Event(line.end.x, 0, (i,)))
 
 
     active_segments = SortedSet([])
@@ -19,52 +19,57 @@ def find_intersections(lines):
         currEvent = q.get()
 
         if currEvent.is_start == 1:
-            active_segments.add((lines[currEvent.id].curr, currEvent.id))
-            curr = active_segments.index((lines[currEvent.id].curr, currEvent.id))
+            active_segments.add((lines[currEvent.id[0]].curr, currEvent.id))
+            curr = active_segments.index((lines[currEvent.id[0]].curr, currEvent.id))
 
             # Sprawdza czy przecina się z sąsiadem pod nim
             if curr > 0:
-                if lines[currEvent.id].does_intersect(lines[active_segments[curr-1][1]]):
-                    if (min(currEvent.id,active_segments[curr-1][1]), max(currEvent.id,active_segments[curr-1][1])) in calculatedPairs:
-                        pass
-                    else:
-                        intersections.append(lines[currEvent.id].intersection_point(lines[active_segments[curr-1][1]]))
-                        calculatedPairs.append((min(currEvent.id,active_segments[curr-1][1]), max(currEvent.id,active_segments[curr-1][1])))
-
-                        if lines[currEvent.id].start.y < lines[active_segments[curr-1][1]].start.y:
-                            q.put(Event(intersections[-1].x, 2, (currEvent.id, active_segments[curr-1][1])))
+                for neighbourId in active_segments[curr-1][1]:
+                    if neighbourId == currEvent.id[0]:
+                        continue
+                    if lines[currEvent.id[0]].does_intersect(lines[neighbourId]):
+                        if (min(currEvent.id[0], neighbourId), max(currEvent.id[0], neighbourId)) in calculatedPairs:
+                            pass
                         else:
-                            q.put(Event(intersections[-1].x, 2, (active_segments[curr-1][1], currEvent.id)))
+                            intersections.append(lines[currEvent.id[0]].intersection_point(lines[neighbourId]))
+                            calculatedPairs.append((min(currEvent.id[0], neighbourId), max(currEvent.id[0], neighbourId)))
+
+                            if lines[currEvent.id[0]].start.y < lines[neighbourId].start.y:
+                                q.put(Event(intersections[-1].x, 2, (currEvent.id[0], neighbourId)))
+                            else:
+                                q.put(Event(intersections[-1].x, 2, (neighbourId, currEvent.id[0])))
 
             # Sprawdza czy przecina się z sąsiadem nad nim
             if curr < len(active_segments)-1:
-                if lines[currEvent.id].does_intersect(lines[active_segments[curr+1][1]]):
-                    if (min(currEvent.id, active_segments[curr+1][1]),
-                                                max(currEvent.id, active_segments[curr+1][1])) in calculatedPairs:
-                        pass
-                    else:
-                        intersections.append(lines[currEvent.id].intersection_point(lines[active_segments[curr+1][1]]))
-                        calculatedPairs.append((min(currEvent.id, active_segments[curr+1][1]),
-                                                max(currEvent.id, active_segments[curr+1][1])))
-
-                        if lines[currEvent.id].start.y < lines[active_segments[curr+1][1]].start.y:
-                            q.put(Event(intersections[-1].x, 2, (currEvent.id, active_segments[curr+1][1])))
+                for neighbourId in active_segments[curr+1][1]:
+                    if neighbourId == currEvent.id[0]:
+                        continue
+                    if lines[currEvent.id[0]].does_intersect(lines[neighbourId]):
+                        if (min(currEvent.id[0], neighbourId),
+                                                    max(currEvent.id[0], neighbourId)) in calculatedPairs:
+                            pass
                         else:
-                            q.put(Event(intersections[-1].x, 2, (active_segments[curr+1][1], currEvent.id)))
+                            intersections.append(lines[currEvent.id[0]].intersection_point(lines[neighbourId]))
+                            calculatedPairs.append((min(currEvent.id[0], neighbourId),
+                                                    max(currEvent.id[0], neighbourId)))
+
+                            if lines[currEvent.id[0]].start.y < lines[neighbourId].start.y:
+                                q.put(Event(intersections[-1].x, 2, (currEvent.id[0], neighbourId)))
+                            else:
+                                q.put(Event(intersections[-1].x, 2, (neighbourId, currEvent.id[0])))
 
         elif currEvent.is_start == 0:
-            active_segments.discard((lines[currEvent.id].curr, currEvent.id))
+            active_segments.discard((lines[currEvent.id[0]].curr, currEvent.id))
 
         else:
-            if (lines[currEvent.id[0]].curr, currEvent.id[0]) in active_segments:
-                active_segments.discard((lines[currEvent.id[0]].curr, currEvent.id[0]))
-                lines[currEvent.id[0]].curr = lines[currEvent.id[0]].get_slope() * (currEvent.val + 10**(-5)) + lines[currEvent.id[0]].get_intercept()
-                q.put(Event(currEvent.val + 10**(-5), 1, currEvent.id[0]))
+            #Wyrzucamy stare punkty
+            active_segments.discard((lines[currEvent.id[0]].curr, (currEvent.id[0],)))
+            active_segments.discard((lines[currEvent.id[1]].curr, (currEvent.id[1],)))
 
-            if (lines[currEvent.id[1]].curr, currEvent.id[1]) in active_segments:
-                active_segments.discard((lines[currEvent.id[1]].curr, currEvent.id[1]))
-                lines[currEvent.id[1]].curr = lines[currEvent.id[1]].get_slope() * (currEvent.val + 10**(-5)) + lines[currEvent.id[1]].get_intercept()
-                q.put(Event(currEvent.val + 10**(-5), 1, currEvent.id[1]))
+            # Po czym dodajemy nowy
+            lines[currEvent.id[0]].curr = lines[currEvent.id[0]].get_slope() * currEvent.val + lines[currEvent.id[0]].get_intercept()
+            lines[currEvent.id[1]].curr = lines[currEvent.id[1]].get_slope() * currEvent.val + lines[currEvent.id[1]].get_intercept()
+            q.put(Event(currEvent.val, 1, currEvent.id))
 
 
     #Restat curr value for every line
